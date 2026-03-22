35cf1f7ad88fa0cd7066831ab75c90c140f712bf
macha <macha@machanoMacBook-Air.local>
2026-03-22 16:48:55 +0900

    Fix site URL to production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://4d7fc128.astro-blog-a1f.pages.dev',
+	site: 'https://astro-blog-a1f.pages.dev',
 	integrations: [mdx(), sitemap()],
 });
